f5b10b73ce15b461ac19eeffcac02c30588dc5ad
hascicm <hascicm.mh@gmail.com>
2017-04-10 12:47:54 +0200

    Update RefactorTest.java

--- src/RefactorTest.java
+++ src/RefactorTest.java
@@ -1,11 +1,7 @@
 public class RefactorTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-	
-	private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
-		try {
+	try {
 			System.out.println("Testing");
 		} catch(Exception e) {
 			
@@ -16,5 +12,9 @@ public class RefactorTest {
 		} catch(Exception e) {
 			throw e;
 		}
+	}
+	
+	private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
+
 	}
 }
