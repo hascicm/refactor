3f420f54fa5a3aa2a2819e63c78d1c63fc9c0d47
hascicm <hascicm.mh@gmail.com>
2017-11-14 23:21:03 +0100

    Update and rename FE.java to large.java

--- src/FE.java
+++
@@ -1,8 +0,0 @@
-package sk.fiit.dp.refactor;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-public class App extends Application {
-int x = call1().call2().call3().call4();
-}
---
+++ src/large.java
@@ -0,0 +1,72 @@
+package sk.fiit.dp.refactor;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+public class App extends Application {
+int x = call1().call2().call3().call4();
+  
+  public void 1(){
+  }
+  public void 2(){
+  }
+
+  public void 3(){
+  }
+
+  public void 4(){
+  }
+
+  public void 5(){
+  }
+
+  public void 6(){
+  }
+
+  public void 7(){
+  }
+
+  public void 8(){
+  }
+
+  public void 9(){
+  }
+
+  public void 10(){
+  }
+
+  public void 11(){
+  }
+
+  public void 12(){
+  }
+
+  public void 13(){
+  }
+
+  public void 14(){
+  }
+
+  public void 15(){
+  }
+  public void 16(){
+  }
+
+  public void 17(){
+  }
+
+  public void 18(){
+  }
+
+  public void 19(){
+  }
+
+  public void 20(){
+  }
+
+  public void 21(){
+  }
+
+
+
+}
