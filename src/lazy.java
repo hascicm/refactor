0698c451921d26493816a8826df1f95ab6a6f6ba
hascicm <hascicm.mh@gmail.com>
2017-11-14 22:50:12 +0100

    Create lazy.java

---
+++ src/lazy.java
@@ -0,0 +1,22 @@
+package sk.fiit.dp.refactor.model.explanation;
+
+public class RepairExplanationTempObject {
+	private String refactoringCode;
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getRefactoringCode() {
+		return refactoringCode;
+	}
+
+	public void setRefactoringCode(String refactoringCode) {
+		this.refactoringCode = refactoringCode;
+	}
+}
