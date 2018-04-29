0b8e9c32e59d2f02bbfd11cadeaa644ef5359043
hasci <hascicm.mh@gmail.com>
2018-04-29 20:06:32 +0200

    RafactorFix

--- src/lazy.java
+++ src/lazy.java
@@ -1,6 +1,18 @@
 package sk.fiit.dp.refactor.model.explanation;
 
-public class lazyAndData {
+\n//REFACTOR - Lazy Class\n// begining of smell with identificator: LAZC1 
+//SMELL: #SmellType(Lazy Class)
+ 
+//EXPLANATION smellname : Lazy Class 
+//EXPLANATION description :Understanding and maintaining classes always costs time and money. So if a class doesn't do enough to earn your attention, it should be deleted. 
+//EXPLANATION possible repairs :Collapse Hierarchy, Inline Class, 
+ \n//REFACTOR - Data Class\n// begining of smell with identificator: DC1 
+//SMELL: #SmellType(Data Class)
+ 
+//EXPLANATION smellname : Data Class 
+//EXPLANATION description :A data class refers to a class that contains only fields and crude methods for accessing them (getters and setters). These are simply containers for data used by other classes. These classes do not contain any additional functionality and cannot independently operate on the data that they own. 
+//EXPLANATION possible repairs :Encapsulate Collection, Encapsulate Field, Hide Method, Remove Setting Method, Extract Method, Move Method, 
+ public class lazyAndData {
 	private String refactoringCode;
 	private String code;
 
@@ -20,3 +32,5 @@ public class lazyAndData {
 		this.refactoringCode = refactoringCode;
 	}
 }
+// end of smell with identificator: DC1 
+// end of smell with identificator: LAZC1 
