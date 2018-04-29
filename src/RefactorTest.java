5d6ae1f1d2c63f409adc1234367534e2a57287d8
hasci <hascicm.mh@gmail.com>
2018-04-29 20:14:01 +0200

    RafactorFix

--- src/RefactorTest.java
+++ src/RefactorTest.java
@@ -5,36 +5,99 @@ public class RefactorTest {
 	public static void main(String[] args) {
 	try {
 			System.out.println("Testing");
-		} catch(Exception e) {
+		} 
+//Refactor - Empty Catch Clausule
+// begining of smell with identificator: ECC1 
+//SMELL: #SmellType(Empty Catch Clausule)
+ 
+//EXPLANATION smellname : Empty Catch Clausule 
+//EXPLANATION description :Catch should at least contain logging mechanism for exception 
+//EXPLANATION possible repairs :Log Exception, 
+ catch(Exception e) {
 			
 		}
+// end of smell with identificator: ECC1 
 		try {
 			System.out.println("Testing");
-		} catch(Exception e) {
+		} 
+//Refactor - Empty Catch Clausule
+// begining of smell with identificator: ECC2 
+//SMELL: #SmellType(Empty Catch Clausule)
+ 
+//EXPLANATION smellname : Empty Catch Clausule 
+//EXPLANATION description :Catch should at least contain logging mechanism for exception 
+//EXPLANATION possible repairs :Log Exception, 
+ catch(Exception e) {
 			
-		}	
+		}
+// end of smell with identificator: ECC2 	
 		
-		try {
-			chain = A().B().C().D().E();
+		
+//REFACTOR - Catch and Rethrow
+// begining of smell with identificator: CR1 
+//SMELL: #SmellType(Catch and Rethrow)
+ 
+//EXPLANATION smellname : Catch and Rethrow 
+//EXPLANATION description :Catching exception and rethrowing it is bad, it should be handled in method which catches the exception. 
+//EXPLANATION possible repairs :Remove Exception Throw, Remove Try-Catch, 
+ try {
+			
+//REFACTOR - Message Chains
+// begining of smell with identificator: MCH1 
+//SMELL: #SmellType(Message Chains)
+ 
+//EXPLANATION smellname : Message Chains 
+//EXPLANATION description :A message chain occurs when a client requests another object, that object requests yet another one, and so on. These chains mean that the client is dependent on navigation along the class structure. Any changes in these relationships require modifying the client. 
+//EXPLANATION possible repairs :Extract Method, Hide Delegate, Move Method, 
+ chain = A().B().C().D().E();
+// end of smell with identificator: MCH1 
 			System.out.println("Testing2");
 		} catch(Exception e) {
 			throw e;
 		}
+// end of smell with identificator: CR1 
 	int i = 5; 
-	i *= 156; 
+	i *= 
+//REFACTOR - Magic Number
+// begining of smell with identificator: MAGIC1 
+//SMELL: #SmellType(Magic Numbers)
+ 
+//EXPLANATION smellname : Magic Numbers 
+//EXPLANATION description :Magic constant should be replaced with constant 
+//EXPLANATION possible repairs :Replace Magic Number with Symbolic Constant, 
+ 156
+// end of smell with identificator: MAGIC1 ; 
 	
 	}
 	
-	private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
+	
+//REFACTOR - Long Parameter List
+// begining of smell with identificator: LPL1 
+//SMELL: #SmellType(Long Parameter List)
+ 
+//EXPLANATION smellname : Long Parameter List 
+//EXPLANATION description :There are too many paramethers for method 
+//EXPLANATION possible repairs :Preserve Whole Object, Replace Parameter with Method, Introduce Parameter Object, 
+ private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
 
 	}
+// end of smell with identificator: LPL1 
 	public class inner {
 		public static void ineerMethod(){
-			try {
+			
+//REFACTOR - Catch and Rethrow
+// begining of smell with identificator: CR2 
+//SMELL: #SmellType(Catch and Rethrow)
+ 
+//EXPLANATION smellname : Catch and Rethrow 
+//EXPLANATION description :Catching exception and rethrowing it is bad, it should be handled in method which catches the exception. 
+//EXPLANATION possible repairs :Remove Exception Throw, Remove Try-Catch, 
+ try {
 				System.out.println("Testing2");
 			} catch(Exception e) {
 				throw e;
 			}
+// end of smell with identificator: CR2 
 		}
 	
 	} 
